89124f9fd1cb9d5f8d6c6422191eef27e3e1ea01
Giulia Rossi <hilervaweautr5@hotmail.com>
2026-06-18 07:27:11 +0200

    Added implement configuration loader with defaults

--- config.py
+++ config.py
@@ -2,31 +2,29 @@ import json
 import os
 
 class ConfigLoader:
-    def __init__(self, default_config):
-        self.default_config = default_config
-        self.config = default_config.copy()
+    def __init__(self, default_config_file, custom_config_file=None):
+        self.default_config_file = default_config_file
+        self.custom_config_file = custom_config_file
+        self.config = self.load_config()
 
-    def load_config(self, file_path):
-        if os.path.exists(file_path):
-            with open(file_path, 'r') as file:
-                try:
-                    user_config = json.load(file)
-                    self.update_config(user_config)
-                except json.JSONDecodeError:
-                    print('Invalid JSON in config file.')
-        else:
-            print('Config file not found. Using defaults.')
+    def load_config(self):
+        defaults = self.load_json(self.default_config_file)
+        custom = self.load_json(self.custom_config_file) if self.custom_config_file else {}
+        return {**defaults, **custom}
 
-    def update_config(self, user_config):
-        for key, value in user_config.items():
-            if key in self.default_config:
-                self.config[key] = value
+    def load_json(self, filename):
+        if not os.path.exists(filename):
+            return {}
+        with open(filename, 'r') as file:
+            return json.load(file)
 
-    def get_config(self):
-        return self.config
+    def get(self, key, default=None):
+        return self.config.get(key, default)
 
-# Example usage:
-# default_config = {'setting1': True, 'setting2': 'default'}
-# config_loader = ConfigLoader(default_config)
-# config_loader.load_config('path/to/config.json')
-# print(config_loader.get_config())
+    def __repr__(self):
+        return json.dumps(self.config, indent=4)
+
+# Example usage (not required for functionality)
+# if __name__ == '__main__':
+#     config_loader = ConfigLoader('default_config.json', 'custom_config.json')
+#     print(config_loader)
