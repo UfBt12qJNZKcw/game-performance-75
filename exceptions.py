310d91f1929e886601b7aaf6e597d844ee17da28
Giulia Rossi <hilervaweautr5@hotmail.com>
2026-07-21 07:09:17 +0200

    Updated define custom game exception classes

--- exceptions.py
+++ exceptions.py
@@ -1,23 +1,27 @@
 class GameError(Exception):
-    """Base class for exceptions in this game."""
+    """Base class for exceptions in the game."""
     pass
 
-class InvalidInputError(GameError):
-    """Raised for invalid inputs."""
-    def __init__(self, message):
+class InvalidMoveError(GameError):
+    """Raised when a move is invalid."""
+    def __init__(self, message="Invalid move attempted."):
         self.message = message
         super().__init__(self.message)
 
-class ResourceNotFoundError(GameError):
-    """Raised when a resource is not found."""
-    def __init__(self, resource):
-        self.resource = resource
-        self.message = f'Resource {resource} not found.'
+class PlayerNotFoundError(GameError):
+    """Raised when a specified player is not found."""
+    def __init__(self, player_name):
+        self.message = f"Player '{player_name}' not found."
         super().__init__(self.message)
 
-class GameLogicError(GameError):
-    """Raised for logical errors within game processing."""
-    def __init__(self, logic_issue):
-        self.logic_issue = logic_issue
-        self.message = f'Game logic error occurred: {logic_issue}'
+class GameAlreadyStartedError(GameError):
+    """Raised when trying to modify a game in progress."""
+    def __init__(self):
+        self.message = "Game has already started."
+        super().__init__(self.message)
+
+class InsufficientResourcesError(GameError):
+    """Raised when a player lacks resources to perform an action."""
+    def __init__(self, resource):
+        self.message = f"Insufficient resources: {resource}."
         super().__init__(self.message)
