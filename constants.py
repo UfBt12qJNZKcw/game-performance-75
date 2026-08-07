90aab78be1ad2e9ed60b47d4e3a34af1d4092c48
Giulia Rossi <hilervaweautr5@hotmail.com>
2026-08-07 07:10:06 +0200

    fix:define game-related constants

--- constants.py
+++ constants.py
@@ -1,39 +1,37 @@
-import math
+MAX_PLAYERS = 100
 
-# Game constants
-FPS = 60
-PLAYER_SPEED = 5.0
-GRAVITY = 9.81
-JUMP_HEIGHT = 1.5
+# Game constants used for scaling and configurations
+FPS_LIMIT = 60
 
-# Color constants
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+# Default screen resolution settings
+SCREEN_RESOLUTION = {
+    'width': 1920,
+    'height': 1080
+}
 
-# Screen dimensions
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+# Game state constants
+GAME_STATES = {
+    'MENU': 0,
+    'PLAYING': 1,
+    'PAUSED': 2,
+    'GAME_OVER': 3
+}
 
-# Level constants
-MAX_LEVELS = 10
-BOSS_HEALTH = 1000
+# Color constants in RGB format
+COLORS = {
+    'WHITE': (255, 255, 255),
+    'BLACK': (0, 0, 0),
+    'RED': (255, 0, 0),
+    'GREEN': (0, 255, 0),
+    'BLUE': (0, 0, 255)
+}
 
-# Utility functions
+# Game physics constants
+GRAVITY = 9.81  # m/s²
+JUMP_FORCE = 15  # arbitrary units
 
-def calculate_distance(point1, point2):
-    return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
-
-
-def clamp(value, min_value, max_value):
-    return max(min_value, min(value, max_value))
-
-
-def radians_to_degrees(radians):
-    return radians * (180.0 / math.pi)
-
-
-def degrees_to_radians(degrees):
-    return degrees * (math.pi / 180.0)
+# Audio volume levels
+SOUND_VOLUMES = {
+    'MUSIC': 0.5,
+    'SFX': 0.7
+}
